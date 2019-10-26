22d6197bd2cfe9713b974cb5922bb94caa7817c7
sarah-am <sarahq.aman@gmail.com>
2019-10-26 19:05:16 +0300

    Mode choice done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { useState } from "react";
 import "./App.css";
+import Home from "./Components/Home";
 import Difficulty from "./Components/Difficulty";
 import Game from "./Components/Game";
 
 function App() {
+  const [mode, setMode] = useState(null); //1
   const [difficulty, setDifficulty] = useState(null);
+
+  const page = () => {
+    if (difficulty) return <Game difficulty={difficulty} />;
+    if (mode) return <Difficulty setDifficulty={setDifficulty} />;
+    return <Home setMode={setMode} />;
+  }; //2
 
   return (
     <div className="App border my-5">
-      {difficulty ? (
-        <Game difficulty={difficulty} />
-      ) : (
-        <Difficulty setDifficulty={setDifficulty} />
-      )}
+      {page()} {/*3*/}
+      <div className="row">
+        <div className="col-3">
+          <button
+            className="btn btn-danger mb-3"
+            onClick={() => {
+              setMode(null);
+              setDifficulty(null);
+            }}
+          >
+            Reset
+          </button>{" "}
+          {/* 4 */}
+        </div>
+      </div>
     </div>
   );
 }
