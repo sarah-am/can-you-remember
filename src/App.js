ce297c4d49f76963bac7459c3abd677887007ab9
sarah-am <sarahq.aman@gmail.com>
2019-10-26 17:53:50 +0300

    Done with randomizing card behavior

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
-import React from "react";
 import "./App.css";
+import React, { useState, useEffect } from "react";
 
-//Data
-import cards from "./data";
+// Data
+import allCards from "./data";
 
-// Card Back Image
-import cardBack from "./images/AppleCard.jpg";
+// Utils
+import { shuffle } from "./utils";
 
-//Components
-import Card from "./Components/Card"; /* 1 */
+// Components
+import Card from "./Components/Card";
 
-function App() {
-  let cardsGrid = cards.map(card => (
-    <Card key={card.id} card={card} />
-  )); /* 2, 3 */
+const App = () => {
+  const [cards, setCards] = useState([]);
+  const [difficulty, setDifficulty] = useState("easy"); //1
+
+  //Used to duplicate the amount of cards since we need two of each and shuffle them using the function defined at the top
+  let cardsTemp = allCards;
+  switch (difficulty) {
+    case "easy":
+      cardsTemp = allCards.slice(0, 6);
+      break;
+    case "medium":
+      cardsTemp = allCards.slice(0, 8);
+      break;
+    default:
+      break;
+  }
+
+  setCards(() => shuffle([...cardsTemp, ...cardsTemp])); //2
+
+  //Mapping through the array of cards and placing them in the card component
+  const cardList = cards.map((card, idx) => (
+    <Card key={`${card.id}-${idx}`} card={card} />
+  ));
 
   return (
     <div className="App border my-5">
       <div className="container">
-        <div className="row">{cardsGrid}</div>
+        <div className="row">
+          <div className="col-9">
+            <div className="row border">{cardList}</div>
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
